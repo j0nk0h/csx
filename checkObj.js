720496bcc5c51396a7045fd67e37eefa7b71b625
j0nk0h <jonkoh39@gmail.com>
2022-04-04 19:50:51 -0700

    solved checkObj

--- checkObj.js
+++ checkObj.js
@@ -1,10 +1,15 @@
 const checkObj = {
-    oddNum: 1,
-    evenNum: 2,
-    foundNum: 5,
-    randomNum: 18
-  };
-  
-  let found = 0;
-  // ADD CODE HERE
-  console.log(found)
+  oddNum: 1,
+  evenNum: 2,
+  foundNum: 5,
+  randomNum: 18
+};
+
+let found = 0;
+// ADD CODE HERE
+for (let key in checkObj) {
+  if (checkObj[key] === 1) {
+    found = 1
+  } 
+}
+console.log(found)
