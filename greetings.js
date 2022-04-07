16d85f0425a4d09024b5bf849d562ba85bb831f7
j0nk0h <jonkoh39@gmail.com>
2022-04-06 20:23:49 -0700

    solved greetings

---
+++ greetings.js
@@ -0,0 +1,17 @@
+function greetings(hour) {
+  // ADD CODE HERE
+  if (hour < 12) {
+    return 'Good Morning!'
+  } else if (hour < 15) {
+    return 'Good Afternoon!'
+  } else {
+    return 'Good Night!'
+  }
+}
+
+// Uncomment these to check your work!
+console.log(greetings(8)); // expected log 'Good Morning!'
+console.log(greetings(12)); // expected log 'Good Afternoon!'
+console.log(greetings(14)); // expected log 'Good Afternoon!'
+console.log(greetings(15)); // expected log 'Good Night!'
+console.log(greetings(18)); // expected log 'Good Night!'
