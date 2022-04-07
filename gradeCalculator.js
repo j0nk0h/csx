f28563fd57f22a255de2b46d09bb6018c759bbb0
j0nk0h <jonkoh39@gmail.com>
2022-04-06 20:32:15 -0700

    solved gradecalculator

---
+++ gradeCalculator.js
@@ -0,0 +1,21 @@
+function gradeCalculator(grade) {
+  // ADD CODE HERE
+  if (grade >= 90) {
+    return "A"
+  } else if (grade >= 80) {
+    return "B"
+  } else if (grade >= 70) {
+    return "C"
+  } else if (grade >= 60) {
+    return "D"
+  } else {
+    return "F"
+  }
+}
+
+// Uncomment the lines below to test your code
+console.log(gradeCalculator(92)); // => "A"
+console.log(gradeCalculator(84)); // => "B"
+console.log(gradeCalculator(70)); // => "C"
+console.log(gradeCalculator(61)); // => "D"
+console.log(gradeCalculator(43)); // => "F"
