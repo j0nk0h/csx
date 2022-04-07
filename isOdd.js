42f77f50bce150779c6353a05c0eb4ca877ad40d
j0nk0h <jonkoh39@gmail.com>
2022-04-06 19:02:53 -0700

    solved isodd

---
+++ isOdd.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+const isOdd = num => {
+    return num % 2 !== 0
+  }
+  // Uncomment these to check your work!
+  console.log(isOdd(5)); // should log: true
+  console.log(isOdd(2008)); // should log: false
