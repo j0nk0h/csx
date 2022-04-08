b0a2e7fe1d265f181920d786aa56314e5fb1f511
j0nk0h <jonkoh39@gmail.com>
2022-04-07 20:37:52 -0700

    solved whileLoopCounter

---
+++ whileLoopCounter.js
@@ -0,0 +1,16 @@
+function imAboutToExplodeWithExcitement(n){
+  // ADD CODE HERE
+  while (n > 0){
+    if (n === 5) {
+      console.log("Oh wow, I can't handle the anticipation");
+    } else if (n === 3) {
+      console.log("I'm about to explode with excitement!")
+    } else {
+      console.log(n)
+    }
+    n--;
+  }
+  console.log("That was kind of a let down")
+}
+
+// Uncomment the line below to check your work!
