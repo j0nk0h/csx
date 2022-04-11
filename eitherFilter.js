7fdad6b054f107487ac71f4206a7bf503a757a4e
j0nk0h <jonkoh39@gmail.com>
2022-04-10 21:47:44 -0700

    added eitherFilter

---
+++ eitherFilter.js
@@ -0,0 +1,9 @@
+function eitherFilter(array, callback1, callback2) {
+  // ADD CODE HERE
+  
+}
+
+// Uncomment these to check your work!
+// const arrOfNums = [10, 35, 105, 9];
+// const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+// const over100 = n => n > 100;
