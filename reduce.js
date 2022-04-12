8f9a1eb5b772932c40716995a9d97ebcedd47388
j0nk0h <jonkoh39@gmail.com>
2022-04-11 19:43:10 -0700

    solved reduce

--- reduce.js
+++ reduce.js
@@ -3,3 +3,15 @@
 // Uncomment these to check your work!
 // const nums = [4, 1, 3];
 // const add = function(a, b) { return a + b; }
+// ADD CODE HERE
+const reduce = (array, cb, initial) => {
+  let acc = initial;
+  for (let i = 0; i < array.length; i++) {
+    acc = cb(acc, array[i])
+  }
+  return acc;
+}
+
+// Uncomment these to check your work!
+const nums = [4, 1, 3];
+const add = function(a, b) { return a + b; }
