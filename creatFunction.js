6ebbe3519a3da81928a23095704e07d62cb0d216
j0nk0h <jonkoh39@gmail.com>
2022-04-12 22:41:15 -0700

    solved createfunction

---
+++ creatFunction.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+const createFunction = () => {
+  return () => {
+  	return 'hello world'
+  }
+}
+
+// Uncomment these to check your work!
+const myFunction = createFunction(); // () => { return 'hello world' }
