c7db06c54016253cbdc9fd32839f2fc6dfd9158f
j0nk0h <jonkoh39@gmail.com>
2022-04-13 22:03:14 -0700

    solved censor

---
+++ censor.js
@@ -0,0 +1,28 @@
+// ADD CODE HERE
+const censor = () => {
+  const savedPair = {};
+  return (arg1, arg2) => {
+    if (arg2 === undefined) {
+      for (let key in savedPair) {
+        arg1 = arg1.replace(key, savedPair[key])
+      }
+      return arg1;
+    }
+    savedPair[arg1] = arg2
+  }
+}
+
+// Uncomment these to check your work!
+const changeScene = censor();
+changeScene('dogs', 'cats');
+changeScene('quick', 'slow');
+console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // should log: 'The slow, brown fox jumps over the lazy cats.'
+/* savedPair = { 'dogs': 'cats', 'quick': 'slow' } 
+(arg1, arg2) => {
+    if (arg2 === undefined) {
+      for (let key in savedPair) {
+        return arg1.replace(key, savedPair[key])
+      }
+    }
+    savedPair[arg1] = arg2
+  }
