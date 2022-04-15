11e5ce6757387724bf2a381326e6809df8183867
j0nk0h <jonkoh39@gmail.com>
2022-04-14 22:31:03 -0700

    added repeater

---
+++ repeater.js
@@ -0,0 +1,10 @@
+console.log('Hello, world!');
+
+function repeater(char) {
+
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(repeater('g'));
+console.log(repeater('j'));
+
