7e526f7042c7f520e36f482b546106a8d98d089e
j0nk0h <jonkoh39@gmail.com>
2022-04-15 22:43:43 -0700

    added factorial

---
+++ factorial.js
@@ -0,0 +1,8 @@
+function factorial(num) {
+
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+// console.log(factorial(4)); // -> 24
+// console.log(factorial(6)); // -> 720
+
