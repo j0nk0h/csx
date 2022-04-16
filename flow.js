27632b30ce09163516b019523e1773f1ed7138fb
j0nk0h <jonkoh39@gmail.com>
2022-04-15 22:45:49 -0700

    added flow

---
+++ flow.js
@@ -0,0 +1,10 @@
+function flow(input, funcArray) {
+
+}
+
+// To check if you've completed the challenge, uncomment this code!
+// function multiplyBy2(num) { return num * 2; }
+// function add7(num) { return num + 7; }
+// function modulo4(num) { return num % 4; }
+// function subtract10(num) { return num - 10; }
+// const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
