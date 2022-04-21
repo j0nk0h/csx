6a8b8b010dcd6c3c5543996a797ed3cc882d8fa1
j0nk0h <jonkoh39@gmail.com>
2022-04-20 22:11:14 -0700

    solved repeater

--- repeater.js
+++ repeater.js
@@ -8,3 +8,17 @@ function repeater(char) {
 console.log(repeater('g'));
 console.log(repeater('j'));
 
+console.log('Hello, world!');
+
+function repeater(char) { 
+	if (char.length === 5) {
+  	return char;
+  }
+  return repeater(char + char[0]);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(repeater('g'));
+console.log(repeater('j'));
+
+// const testStr = 'gggg'
