8370bb21db1b8505ad1f4303f3676a240eb216d9
j0nk0h <jonkoh39@gmail.com>
2022-04-20 22:13:23 -0700

    solved POW

--- POW.js
+++ POW.js
@@ -4,3 +4,10 @@ function pow(base, exponent) {
 
 // To check if you've completed the challenge, uncomment these console.logs!
 // console.log(pow(2, 4)); // -> 16
+function pow(base, exponent) {
+	if (exponent === 1) return base;
+  return base * pow(base, exponent - 1);  
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(pow(2, 4)); // -> 16
