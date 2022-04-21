dada7f7c54c69cb58d034184f3a7029970a8623e
j0nk0h <jonkoh39@gmail.com>
2022-04-20 22:11:58 -0700

    solved factorial

--- factorial.js
+++ factorial.js
@@ -6,3 +6,12 @@ function factorial(num) {
 // console.log(factorial(4)); // -> 24
 // console.log(factorial(6)); // -> 720
 
+function factorial(num) {
+	if (num === 1) return num;
+
+  return num * factorial(num - 1);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(factorial(4)); // -> 24
+console.log(factorial(6)); // -> 720
