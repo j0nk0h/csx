a3d81e85d804463f2006a20c9291db84f12199d5
j0nk0h <jonkoh39@gmail.com>
2022-04-24 15:36:09 -0700

    solved personconstructor

---
+++ personConstuctor.js
@@ -0,0 +1,10 @@
+function PersonConstructor() {
+	// add code here
+	this.greet = () => {
+  	console.log('hello')   
+  }
+}
+
+const simon = new PersonConstructor();
+
+// Uncomment this line to check your work!
